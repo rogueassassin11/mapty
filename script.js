20b0f6c00e16dd289e26f17b33fa405b7a4a3d08
Mae Cagadas <32762531+rogueassassin11@users.noreply.github.com>
2021-05-14 22:31:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+/*****************************/
+/* PROJECT PLANNING
+/*****************************/
+
+// 1. USER STORY - description of the application's functionality from the user's perspective. When put together, it should describe the entire application. "As a [type of user], I want [action], so that [benefit]"
+
+// 2 + 3 FEATURES + FLOWCHART - what we will build -  good to start with events
+
+// 4. Architecture - how we will build it
+
+// 5. Development - implementation of the plan using code
+
+/****************************/
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      const coords = [latitude, longitude];
+
+      //LEAFLET
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
